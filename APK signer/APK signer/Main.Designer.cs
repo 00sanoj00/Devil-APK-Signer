1906472c8d4a3fd341cb28662804e2d7d009310b
00sanoj00 <00sanoj00@gmail.com>
2020-04-13 11:03:55 +0530

    internal release
    internal release

---
+++ APK signer/APK signer/Main.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace APK_Signer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monoFlat_ThemeContainer1 = new MonoFlat.MonoFlat_ThemeContainer();
+            this.monoFlat_Label2 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_Separator2 = new MonoFlat.MonoFlat_Separator();
+            this.monoFlat_Separator1 = new MonoFlat.MonoFlat_Separator();
+            this.monoFlat_Button1 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Label1 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_TextBox1 = new MonoFlat.MonoFlat_TextBox();
+            this.monoFlat_ControlBox1 = new MonoFlat.MonoFlat_ControlBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.monoFlat_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer1
+            // 
+            this.monoFlat_ThemeContainer1.AllowDrop = true;
+            this.monoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Separator2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Separator1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_TextBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_ControlBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.textBox1);
+            this.monoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer1.Name = "monoFlat_ThemeContainer1";
+            this.monoFlat_ThemeContainer1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer1.RoundCorners = true;
+            this.monoFlat_ThemeContainer1.Sizable = true;
+            this.monoFlat_ThemeContainer1.Size = new System.Drawing.Size(561, 579);
+            this.monoFlat_ThemeContainer1.SmartBounds = true;
+            this.monoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.monoFlat_ThemeContainer1.TabIndex = 0;
+            this.monoFlat_ThemeContainer1.Text = "Devil APK SIGNER";
+            this.monoFlat_ThemeContainer1.DragEnter += new System.Windows.Forms.DragEventHandler(this.monoFlat_ThemeContainer1_DragEnter);
+            // 
+            // monoFlat_Label2
+            // 
+            this.monoFlat_Label2.AutoSize = true;
+            this.monoFlat_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monoFlat_Label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(125)))), ((int)(((byte)(132)))));
+            this.monoFlat_Label2.Location = new System.Drawing.Point(22, 119);
+            this.monoFlat_Label2.Name = "monoFlat_Label2";
+            this.monoFlat_Label2.Size = new System.Drawing.Size(517, 47);
+            this.monoFlat_Label2.TabIndex = 8;
+            this.monoFlat_Label2.Text = "Drag and Drop APK Files Here";
+            // 
+            // monoFlat_Separator2
+            // 
+            this.monoFlat_Separator2.Location = new System.Drawing.Point(16, 83);
+            this.monoFlat_Separator2.Name = "monoFlat_Separator2";
+            this.monoFlat_Separator2.Size = new System.Drawing.Size(531, 10);
+            this.monoFlat_Separator2.TabIndex = 7;
+            this.monoFlat_Separator2.Text = "monoFlat_Separator2";
+            // 
+            // monoFlat_Separator1
+            // 
+            this.monoFlat_Separator1.Location = new System.Drawing.Point(16, 194);
+            this.monoFlat_Separator1.Name = "monoFlat_Separator1";
+            this.monoFlat_Separator1.Size = new System.Drawing.Size(531, 10);
+            this.monoFlat_Separator1.TabIndex = 6;
+            this.monoFlat_Separator1.Text = "monoFlat_Separator1";
+            // 
+            // monoFlat_Button1
+            // 
+            this.monoFlat_Button1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button1.Image = null;
+            this.monoFlat_Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button1.Location = new System.Drawing.Point(12, 272);
+            this.monoFlat_Button1.Name = "monoFlat_Button1";
+            this.monoFlat_Button1.Size = new System.Drawing.Size(534, 41);
+            this.monoFlat_Button1.TabIndex = 5;
+            this.monoFlat_Button1.Text = "SIGNER APK";
+            this.monoFlat_Button1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button1.Click += new System.EventHandler(this.monoFlat_Button1_Click);
+            // 
+            // monoFlat_Label1
+            // 
+            this.monoFlat_Label1.AutoSize = true;
+            this.monoFlat_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(125)))), ((int)(((byte)(132)))));
+            this.monoFlat_Label1.Location = new System.Drawing.Point(13, 207);
+            this.monoFlat_Label1.Name = "monoFlat_Label1";
+            this.monoFlat_Label1.Size = new System.Drawing.Size(89, 15);
+            this.monoFlat_Label1.TabIndex = 4;
+            this.monoFlat_Label1.Text = "Commend Line";
+            // 
+            // monoFlat_TextBox1
+            // 
+            this.monoFlat_TextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_TextBox1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.monoFlat_TextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.monoFlat_TextBox1.Image = null;
+            this.monoFlat_TextBox1.Location = new System.Drawing.Point(13, 225);
+            this.monoFlat_TextBox1.MaxLength = 32767;
+            this.monoFlat_TextBox1.Multiline = false;
+            this.monoFlat_TextBox1.Name = "monoFlat_TextBox1";
+            this.monoFlat_TextBox1.ReadOnly = false;
+            this.monoFlat_TextBox1.Size = new System.Drawing.Size(536, 41);
+            this.monoFlat_TextBox1.TabIndex = 2;
+            this.monoFlat_TextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.monoFlat_TextBox1.UseSystemPasswordChar = false;
+            this.monoFlat_TextBox1.TextChanged += new System.EventHandler(this.monoFlat_TextBox1_TextChanged);
+            // 
+            // monoFlat_ControlBox1
+            // 
+            this.monoFlat_ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.monoFlat_ControlBox1.EnableHoverHighlight = false;
+            this.monoFlat_ControlBox1.EnableMaximizeButton = true;
+            this.monoFlat_ControlBox1.EnableMinimizeButton = true;
+            this.monoFlat_ControlBox1.Location = new System.Drawing.Point(449, 15);
+            this.monoFlat_ControlBox1.Name = "monoFlat_ControlBox1";
+            this.monoFlat_ControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.monoFlat_ControlBox1.TabIndex = 1;
+            this.monoFlat_ControlBox1.Text = "monoFlat_ControlBox1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(13, 319);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(536, 248);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "\r\n\r\n\r\n\r\n\r\n🌼╱▓█▔▔▔▔▔▔▔█▓╲🌼\r\n🌼❥❥══❥Devil APK Signer 🌿🌼🌻\r\n🌼╲▓█▃▃▃▃▃▃▃█▓╱🌼\r\n\r" +
+    "\n🌼╱▓█▔▔▔▔▔▔▔█▓╲🌼\r\n🌼❥❥══❥     V1.0(Beta)       🌿🌼🌻\r\n🌼╲▓█▃▃▃▃▃▃▃█▓╱🌼";
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.ClientSize = new System.Drawing.Size(561, 579);
+            this.Controls.Add(this.monoFlat_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Devil APK SIGNER";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.monoFlat_ThemeContainer1.ResumeLayout(false);
+            this.monoFlat_ThemeContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_ThemeContainer monoFlat_ThemeContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MonoFlat.MonoFlat_ControlBox monoFlat_ControlBox1;
+        private MonoFlat.MonoFlat_TextBox monoFlat_TextBox1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button1;
+        private MonoFlat.MonoFlat_Label monoFlat_Label1;
+        private MonoFlat.MonoFlat_Label monoFlat_Label2;
+        private MonoFlat.MonoFlat_Separator monoFlat_Separator2;
+        private MonoFlat.MonoFlat_Separator monoFlat_Separator1;
+    }
+}
---
+++ APK signer/APK signer/Main.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APK_Signer
+{
+    public partial class Main : Form
+    {
+
+        StreamWriter stdin = null;
+
+        public Main()
+        {
+           
+            InitializeComponent();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+            textBox1.Enabled = false;
+            monoFlat_TextBox1.Enabled = false;
+          
+        }
+
+
+        private void monoFlat_ThemeContainer1_DragEnter(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                if(monoFlat_TextBox1.Text.Equals("")) {
+                    textBox1.Text = "";
+                    textBox1.ForeColor = Color.Red;
+
+                    monoFlat_TextBox1.Text = "java -jar u.jar --apks " + files[0];
+                    textBox1.Text += files[0] + Environment.NewLine;
+                }else{
+                    monoFlat_TextBox1.Text += " "+files[0];
+                    textBox1.Text += files[0] + Environment.NewLine;
+                    monoFlat_Button1.Text = "SIGNER APKS";
+                } 
+            }
+        }
+
+
+        private void monoFlat_Button1_Click(object sender, EventArgs e)
+        {
+
+            textBox1.ForeColor = Color.LawnGreen;
+            textBox1.Text = "";
+            StartCmdProcess();
+            //stdin.WriteLine();
+            stdin.Write(monoFlat_TextBox1.Text + Environment.NewLine);
+            stdin.WriteLine();
+            
+        }
+        private void StartCmdProcess()
+        {
+            ProcessStartInfo pStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "start /WAIT",
+                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            Process cmdProcess = new Process
+            {
+                StartInfo = pStartInfo,
+                EnableRaisingEvents = true,
+            };
+
+            cmdProcess.Start();
+            cmdProcess.BeginErrorReadLine();
+            cmdProcess.BeginOutputReadLine();
+            stdin = cmdProcess.StandardInput;
+
+            cmdProcess.OutputDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new MethodInvoker(() =>
+                    {
+                        textBox1.AppendText(evt.Data + Environment.NewLine);
+                        textBox1.ScrollToCaret();
+                        monoFlat_TextBox1.Text = "";
+                    }));
+                }
+            };
+
+            cmdProcess.ErrorDataReceived += (s, evt) =>
+            {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new Action(() =>
+                    {
+                        //rtbStdErr.AppendText(evt.Data + Environment.NewLine);
+                        //rtbStdErr.ScrollToCaret();
+                    }));
+                }
+            };
+
+            cmdProcess.Exited += (s, evt) =>
+            {
+                // cmdProcess?.Dispose();
+            };
+        }
+
+
+        private void monoFlat_TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+        }
+
+
+
+    }
+}
